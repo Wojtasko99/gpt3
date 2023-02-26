6cec339221c34c4f43be42b74d1d4ebec5f37256
Damian Wojtal <106908962+Wojtasko99@users.noreply.github.com>
2023-02-26 17:04:58 +0100

    Added brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './brand.css'
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
 const Brand = () => {
   return (
-    <div>
+    <div className='brand__container section__padding'>
+      <img className='brand__container-img' alt="google" src={google}></img>
+      <img className='brand__container-img' alt="slack" src={slack}></img>
+      <img className='brand__container-img' alt="atlassian" src={atlassian}></img>
+      <img className='brand__container-img' alt="dropbox" src={dropbox}></img>
+      <img className='brand__container-img' alt="shopify" src={shopify}></img>
       
     </div>
   )
