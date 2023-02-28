d81ff853c550a6f137a08f737b5312dcd146dd2b
Damian Wojtal <106908962+Wojtasko99@users.noreply.github.com>
2023-02-28 11:31:26 +0100

    Added blog section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './article.css';
-
-const Article = () => {
+const Article = (props) => {
   return (
-    <div>
-      
+    <div className='gpt3__article section__margin'>
+      <div className='gpt3__article-image'>
+        <img alt="article_img" src={props.img} />
+      </div>
+      <div className='gpt3__article-text'>
+        <div>
+          <p>{props.date}</p>
+          <h2>{props.heading}</h2>
+        </div>
+        <p>Read full article</p>
+      </div>
     </div>
   )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './blog.css'
+import {Article} from '../../components/index.js';
+import blog1 from '../../assets/blog01.png';
+import blog2 from '../../assets/blog02.png';
+import blog3 from '../../assets/blog03.png';
+import blog4 from '../../assets/blog04.png';
+import blog5 from '../../assets/blog05.png';
 const Blog = () => {
   return (
-    <div>
-      
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-big-article'>
+          <Article img={blog1} date="Sep 26, 2021" heading="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+        <div className='gpt3__blog-small-articles'>
+          <Article img={blog2} date="Sep 26, 2021" heading="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article img={blog3} date="Sep 26, 2021" heading="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article img={blog4} date="Sep 26, 2021" heading="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article img={blog5} date="Sep 26, 2021" heading="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+      </div>
     </div>
   )
 }
